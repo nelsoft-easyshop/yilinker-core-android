abbc0b4008032e4e2da9709f69798d818140cde3
rlcoronadoesmobile <rhys.coronado@easyshop.ph>
2015-08-07 21:13:36 +0800

    Added ExpandedGridView customview for home layout.

---
+++ Core/core/src/main/java/com/yilinker/core/customview/ExpandedGridView.java
@@ -0,0 +1,50 @@
+package com.yilinker.core.customview;
+
+/**
+ * Created by rlcoronado on 8/6/15.
+ */
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.ViewGroup;
+import android.widget.GridView;
+
+public class ExpandedGridView extends GridView {
+
+    boolean expanded = false;
+
+    public ExpandedGridView(Context context) {
+        super(context);
+    }
+
+    public ExpandedGridView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public ExpandedGridView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    public boolean isExpanded() {
+        return expanded;
+    }
+
+    @Override
+    public void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+
+        if (isExpanded()) {
+
+            int expandSpec = MeasureSpec.makeMeasureSpec(MEASURED_SIZE_MASK,
+                    MeasureSpec.AT_MOST);
+            super.onMeasure(widthMeasureSpec, expandSpec);
+
+            ViewGroup.LayoutParams params = getLayoutParams();
+            params.height = getMeasuredHeight();
+        } else {
+            super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        }
+    }
+
+    public void setExpanded(boolean expanded) {
+        this.expanded = expanded;
+    }
+}
