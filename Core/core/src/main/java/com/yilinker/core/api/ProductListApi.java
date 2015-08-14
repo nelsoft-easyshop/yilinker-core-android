5c9bf4c3f50dce5c851ade005c48393d25f1dafb
PatrickVillanueva <patrick.villanueva@easyshop.ph>
2015-08-14 13:46:45 +0800

    Added initial commit for productlist api request

--- Core/core/src/main/java/com/yilinker/core/api/ProductListApi.java
+++ Core/core/src/main/java/com/yilinker/core/api/ProductListApi.java
@@ -22,46 +22,11 @@ import java.util.List;
  */
 public class ProductListApi {
 
-    public static Request getProductList(final int requestCode, String target,
-                                         int priceFrom, int priceTo,int categoryId,int sellerId,
-                                         String selectedSort, String selectedDirection,
-                                         int brandId, int loadPage, final ResponseHandler responseHandler) {
-        String url;
-        if (target.equals("")){
-            url = String.format("%s/%s/%s?" +
-                            "%s=%s" +"&%s=%s" +"&%s=%s" +"&%s=%s" +"&%s=%s" +"&%s=%s" +"&%s=%s" +"&%s=%s" +"&%s=%s",
-                    APIConstants.DOMAIN, APIConstants.PRODUCT_API,
-                    APIConstants.PRODUCT_GET_LIST,
-                    APIConstants.PRODUCT_lIST_PRICE_FROM, Integer.toString(priceFrom),
-                    APIConstants.PRODUCT_LIST_PRICE_TO,Integer.toString(priceTo),
-                    APIConstants.PRODUCT_LIST_CATEGORY_ID,Integer.toString(categoryId),
-                    APIConstants.PRODUCT_lIST_SELLER_ID, Integer.toString(sellerId),
-                    APIConstants.PRODUCT_LIST_SORT_TYPE, selectedSort,
-                    APIConstants.PRODUCT_LIST_SORT_DIRECTION, selectedDirection,
-//                    APIConstants.PRODUCT_lIST_FILTER, filterGroups,
-                    APIConstants.PRODUCT_LIST_BRAND_ID,Integer.toString(brandId),
-                    APIConstants.PRODUCT_LIST_PAGE, Integer.toString(loadPage));
+    public static Request getProductList(final int requestCode, String target,final ResponseHandler responseHandler) {
+        String url= String.format("%s/%s" ,
+                    APIConstants.DOMAIN,
+                    target);
 
-        }else{
-//            url = String.format("%s/%s/%s?" +
-//                            "%s=%s" +"&%s=%s" +"&%s=%s" +"&%s=%s" +"&%s=%s" +"&%s=%s" +"&%s=%s" +"&%s=%s" +"&%s=%s",
-//                    APIConstants.DOMAIN, APIConstants.PRODUCT_API,
-//                    target,
-//                    APIConstants.PRODUCT_lIST_PRICE_FROM, Integer.toString(priceFrom),
-//                    APIConstants.PRODUCT_LIST_PRICE_TO,Integer.toString(priceTo),
-//                    APIConstants.PRODUCT_LIST_CATEGORY_ID,Integer.toString(categoryId),
-//                    APIConstants.PRODUCT_lIST_SELLER_ID, Integer.toString(sellerId),
-//                    APIConstants.PRODUCT_LIST_SORT_TYPE, selectedSort,
-//                    APIConstants.PRODUCT_LIST_SORT_DIRECTION, selectedDirection,
-////                    APIConstants.PRODUCT_lIST_FILTER, filterGroups,
-//                    APIConstants.PRODUCT_LIST_BRAND_ID,Integer.toString(brandId),
-//                    APIConstants.PRODUCT_LIST_PAGE, Integer.toString(loadPage));
-
-            url = String.format("%s/%s/%s" ,
-                    APIConstants.DOMAIN, APIConstants.PRODUCT_API,
-                    APIConstants.PRODUCT_GET_LIST);
-
-        }
 
         Request request = new JsonObjectRequest(url, null, new Response.Listener<JSONObject>() {
             @Override
@@ -88,4 +53,70 @@ public class ProductListApi {
         return request;
 
     }
+//    public static Request getProductList(final int requestCode, String target,
+//                                         int priceFrom, int priceTo,int categoryId,int sellerId,
+//                                         String selectedSort, String selectedDirection,
+//                                         int brandId, int loadPage, final ResponseHandler responseHandler) {
+//        String url;
+//        if (target.equals("")){
+//            url = String.format("%s/%s/%s?" +
+//                            "%s=%s" +"&%s=%s" +"&%s=%s" +"&%s=%s" +"&%s=%s" +"&%s=%s" +"&%s=%s" +"&%s=%s" +"&%s=%s",
+//                    APIConstants.DOMAIN, APIConstants.PRODUCT_API,
+//                    APIConstants.PRODUCT_GET_LIST,
+//                    APIConstants.PRODUCT_lIST_PRICE_FROM, Integer.toString(priceFrom),
+//                    APIConstants.PRODUCT_LIST_PRICE_TO,Integer.toString(priceTo),
+//                    APIConstants.PRODUCT_LIST_CATEGORY_ID,Integer.toString(categoryId),
+//                    APIConstants.PRODUCT_lIST_SELLER_ID, Integer.toString(sellerId),
+//                    APIConstants.PRODUCT_LIST_SORT_TYPE, selectedSort,
+//                    APIConstants.PRODUCT_LIST_SORT_DIRECTION, selectedDirection,
+////                    APIConstants.PRODUCT_lIST_FILTER, filterGroups,
+//                    APIConstants.PRODUCT_LIST_BRAND_ID,Integer.toString(brandId),
+//                    APIConstants.PRODUCT_LIST_PAGE, Integer.toString(loadPage));
+//
+//        }else{
+////            url = String.format("%s/%s/%s?" +
+////                            "%s=%s" +"&%s=%s" +"&%s=%s" +"&%s=%s" +"&%s=%s" +"&%s=%s" +"&%s=%s" +"&%s=%s" +"&%s=%s",
+////                    APIConstants.DOMAIN, APIConstants.PRODUCT_API,
+////                    target,
+////                    APIConstants.PRODUCT_lIST_PRICE_FROM, Integer.toString(priceFrom),
+////                    APIConstants.PRODUCT_LIST_PRICE_TO,Integer.toString(priceTo),
+////                    APIConstants.PRODUCT_LIST_CATEGORY_ID,Integer.toString(categoryId),
+////                    APIConstants.PRODUCT_lIST_SELLER_ID, Integer.toString(sellerId),
+////                    APIConstants.PRODUCT_LIST_SORT_TYPE, selectedSort,
+////                    APIConstants.PRODUCT_LIST_SORT_DIRECTION, selectedDirection,
+//////                    APIConstants.PRODUCT_lIST_FILTER, filterGroups,
+////                    APIConstants.PRODUCT_LIST_BRAND_ID,Integer.toString(brandId),
+////                    APIConstants.PRODUCT_LIST_PAGE, Integer.toString(loadPage));
+//
+//            url = String.format("%s/%s/%s" ,
+//                    APIConstants.DOMAIN, APIConstants.PRODUCT_API,
+//                    APIConstants.PRODUCT_GET_LIST);
+//
+//        }
+//
+//        Request request = new JsonObjectRequest(url, null, new Response.Listener<JSONObject>() {
+//            @Override
+//            public void onResponse(JSONObject response) {
+//
+//                Gson gson = GsonUtility.createGsonBuilder(APIResponse.class, new APIResponse.APIResponseInstance()).create();
+//                ProductList obj = gson.fromJson(response.toString(), ProductList.class);
+//
+////                Gson gson = GsonUtility.createGsonBuilder(ProductList.class, new ProductList.ProductListInstance()).create();
+////                String jsonString = new Gson().toJson(apiResponse.getData());
+//                //Cart obj = gson.fromJson(jsonString, Cart.class);
+//
+//                responseHandler.onSuccess(requestCode, obj);
+//
+//            }
+//        }, new Response.ErrorListener() {
+//
+//            @Override
+//            public void onErrorResponse(VolleyError error) {
+//                responseHandler.onFailed(requestCode, APIConstants.API_CONNECTION_PROBLEM);
+//            }
+//        });
+//
+//        return request;
+//
+//    }
 }
