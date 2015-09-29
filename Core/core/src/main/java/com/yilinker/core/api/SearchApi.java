8f831f49c62456669aa542475dde1ab32b543cc6
yrbbry <edward.abergas@easyshop.ph>
2015-09-30 02:35:22 +0800

    Modified Search and Transaction API.

--- Core/core/src/main/java/com/yilinker/core/api/SearchApi.java
+++ Core/core/src/main/java/com/yilinker/core/api/SearchApi.java
@@ -16,12 +16,18 @@ import com.yilinker.core.constants.APIConstants;
 import com.yilinker.core.interfaces.ResponseHandler;
 import com.yilinker.core.model.APIResponse;
 import com.yilinker.core.model.Search;
+import com.yilinker.core.model.TransactionList;
+import com.yilinker.core.model.seller.CategoryProducts;
 import com.yilinker.core.model.seller.SearchTransaction;
+import com.yilinker.core.model.seller.SearchTransactionList;
 import com.yilinker.core.utility.GsonUtility;
 import com.yilinker.core.utility.SocketTimeout;
 
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.UnsupportedEncodingException;
 import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
@@ -107,7 +113,6 @@ public class SearchApi {
 
     }
 
-
     public static Request searchTransaction(final int requestCode, String token, String query, final ResponseHandler responseHandler) {
 
         String url = String.format("%s/%s/%s/%s?%s=%s&%s=%s",
@@ -154,4 +159,106 @@ public class SearchApi {
         return requestGetCart;
 
     }
+
+    public static Request searchProductNameSuggestion(final int requestCode, String token, String query, final ResponseHandler responseHandler) {
+
+        String url = String.format("%s/%s/%s/%s?%s=%s&%s=%s",
+                APIConstants.DOMAIN, APIConstants.AUTH_API, APIConstants.PRODUCT_MANAGEMENT_API, APIConstants.PRODUCT_NAME_API,
+                APIConstants.ACCESS_TOKEN, token,
+                APIConstants.SEARCH_QUERY, query);
+
+        Request requestGetCart = new JsonObjectRequest(url, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+
+                Gson gson = GsonUtility.createGsonBuilder(APIResponse.class, new APIResponse.APIResponseInstance()).create();
+                APIResponse apiResponse = gson.fromJson(response.toString(), APIResponse.class);
+
+                gson = GsonUtility.createGsonBuilder(CategoryProducts.class, new CategoryProducts.CategoryProductsInstance()).create();
+                String jsonString = new Gson().toJson(apiResponse.getData());
+                CategoryProducts[] obj = gson.fromJson(jsonString, CategoryProducts[].class);
+
+
+
+                responseHandler.onSuccess(requestCode, obj);
+
+            }
+        }, new Response.ErrorListener() {
+
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                String message = "An error occured.";
+                if (error instanceof TimeoutError || error instanceof NoConnectionError) {
+                    message = "No connection available.";
+                } else if (error instanceof AuthFailureError) {
+                    message = "Authentication Failure.";
+                } else if (error instanceof ServerError) {
+                    message = "Server error.";
+                } else if (error instanceof NetworkError) {
+                    message = "Network Error.";
+                } else if (error instanceof ParseError) {
+                    message = "Parse error.";
+                }
+                responseHandler.onFailed(requestCode, message);
+            }
+        });
+
+        requestGetCart.setRetryPolicy(SocketTimeout.getRetryPolicy());
+
+        return requestGetCart;
+
+    }
+
+    public static Request getTransactionList(final int requestCode, String accessToken, String productName, final ResponseHandler responseHandler) {
+
+        String endpoint = String.format("%s/%s/%s?%s=%s&%s=%s", APIConstants.DOMAIN, APIConstants.AUTH_API, APIConstants.SELLER_TRANSACTION_LIST_API,
+                APIConstants.ACCESS_TOKEN, accessToken, APIConstants.PRODUCT_PARAMS_PRODUCT_NAME, productName);
+
+        Request request = new JsonObjectRequest(endpoint, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                Gson gson = GsonUtility.createGsonBuilder(APIResponse.class, new APIResponse.APIResponseInstance()).create();
+                APIResponse apiResponse = gson.fromJson(response.toString(), APIResponse.class);
+
+                if(apiResponse.isSuccessful()) {
+
+                    gson = GsonUtility.createGsonBuilder(SearchTransactionList.class, new SearchTransactionList.SearchTransactionListInstance()).create();
+                    String jsonString = new Gson().toJson(apiResponse.getData());
+                    SearchTransactionList obj = gson.fromJson(jsonString, SearchTransactionList.class);
+
+                    responseHandler.onSuccess(requestCode, obj);
+
+                }else{
+
+                    responseHandler.onFailed(requestCode, apiResponse.getMessage());
+
+                }
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+                String message = APIConstants.API_CONNECTION_PROBLEM;
+                try {
+                    String responseBody = new String(error.networkResponse.data, "utf-8" );
+                    JSONObject jsonObject = new JSONObject( responseBody );
+                    jsonObject = jsonObject.getJSONObject("data");
+                    JSONArray errors = jsonObject.getJSONArray("errors");
+                    message = errors.getString(0);
+
+                } catch ( JSONException e ) {
+                    //Handle a malformed json response
+                } catch (UnsupportedEncodingException e){
+
+                }
+                responseHandler.onFailed(requestCode, message);
+            }
+        });
+
+        request.setRetryPolicy(SocketTimeout.getRetryPolicy());
+
+        return request;
+    }
+
 }
