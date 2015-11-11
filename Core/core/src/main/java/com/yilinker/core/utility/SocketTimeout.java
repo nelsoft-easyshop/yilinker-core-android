86d8a3994dc678edc7beee8de6f1a1c0d3286937
rlcoronadoesmobile <rhys.coronado@easyshop.ph>
2015-11-11 18:13:21 +0800

    Modified socket timeout ms from 5000 to 10000.

--- Core/core/src/main/java/com/yilinker/core/utility/SocketTimeout.java
+++ Core/core/src/main/java/com/yilinker/core/utility/SocketTimeout.java
@@ -9,7 +9,7 @@ import com.android.volley.RetryPolicy;
 public class SocketTimeout {
 
     public static RetryPolicy getRetryPolicy(){
-        int socketTimeout = 5000;
+        int socketTimeout = 10000;
         RetryPolicy policy = new DefaultRetryPolicy(socketTimeout,
                 DefaultRetryPolicy.DEFAULT_MAX_RETRIES,
                 DefaultRetryPolicy.DEFAULT_BACKOFF_MULT);
