459cb29afb171da9cea300bf3705b81cb1cb24e3
joanbautista103186 <joan.bautista@easyshop.ph>
2016-06-10 10:01:13 +0800

    Updated the parser for Cart object.

---
+++ Core/core/src/main/java/com/yilinker/core/model/buyer/CartSummary.java
@@ -0,0 +1,53 @@
+package com.yilinker.core.model.buyer;
+
+import com.google.gson.InstanceCreator;
+import com.google.gson.annotations.SerializedName;
+
+import java.lang.reflect.Type;
+import java.util.List;
+
+/**
+ * Created by J.Bautista on 6/8/16.
+ */
+public class CartSummary {
+
+        private List<CartItem2> items;
+        private String totalShippingCost;
+        @SerializedName("totalAmount")
+        private String totalCost;
+
+
+        public List<CartItem2> getItems() {
+            return items;
+        }
+
+        public void setItems(List<CartItem2> items) {
+            this.items = items;
+        }
+
+        public String getTotalShippingCost() {
+            return totalShippingCost;
+        }
+
+        public void setTotalShippingCost(String totalShippingCost) {
+            this.totalShippingCost = totalShippingCost;
+        }
+
+    public String getTotalCost() {
+        return totalCost;
+    }
+
+    public void setTotalCost(String totalCost) {
+        this.totalCost = totalCost;
+    }
+
+    public static class CartSummaryInstance implements InstanceCreator<CartSummary> {
+
+            @Override
+            public CartSummary createInstance(Type type) {
+
+                return new CartSummary();
+            }
+        }
+
+}
